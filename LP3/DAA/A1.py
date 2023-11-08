f59eeeed839de8431128a365cdfa2ae4f6f5556b
Sreyash <sreyashkunkekar2889@gmail.com>
2023-11-09 00:23:45 +0530

    LP3 updated

--- LP3/DAA/A1.py
+++ LP3/DAA/A1.py
@@ -1,7 +1,4 @@
-import timeit
-
-
-def fibonacci(n):
+def non_recursive_fibonacci(n):
     if n < 0:
         raise ValueError("n must be a non-negative integer.")
     elif n == 0 or n == 1:
@@ -15,42 +12,40 @@ def fibonacci(n):
             b = c
         return c
 
+def recursive_fibonacci(n):
+    if n < 0:
+        raise ValueError("n must be a non-negative integer.")
+    elif n == 0 or n == 1:
+        return n
+    else:
+        return recursive_fibonacci(n - 1) + recursive_fibonacci(n - 2)
 
-def fibonacci_recursive(n):
-    """Recursive fibonacci function"""
-    if n == 0:
-        return 0
-    if n == 1:
-        return 1
-    fib_recur_list[n] = fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2)
-    return fib_recur_list[n]
+def analyze_time_complexity(n):
+    non_recursive_time_complexity = "O(n)"
+    recursive_time_complexity = "O(2^n)"
+    return (f"Non-recursive Fibonacci function time complexity: { non_recursive_time_complexity}\n"
+            f"Recursive Fibonacci function time complexity: {recursive_time_complexity}")
 
+def analyze_space_complexity(n):
+    non_recursive_space_complexity = "O(1)"
+    recursive_space_complexity = "O(n)"
+    return (f"Non-recursive Fibonacci function space complexity: {non_recursive_space_complexity}\n"
+            f"Recursive Fibonacci function space complexity: {recursive_space_complexity}") 
 
-N = 20
-RUNS = 1000
-print(f"Given N = {N}\n{RUNS} runs")
+def main():
 
-fib_list = [0] * (N + 1)
-fib_list[0] = 0
-fib_list[1] = 1
-print(
-    "Fibonacci non-recursive:",
-    fibonacci(N),
-    "\tTime:",
-    f'{timeit.timeit("fibonacci(N)", setup=f"from __main__ import fibonacci;N={N}", number=RUNS):5f}',
-    "O(n)\tSpace: O(1)",
-)
+    print("Non-recursive Fibonacci numbers:")
+    for i in range(10):
+        print(non_recursive_fibonacci(i))
 
-fib_recur_list = [0] * (N + 1)
-fib_recur_list[0] = 0
-fib_recur_list[1] = 1
-print(
-    "Fibonacci recursive:\t",
-    fibonacci_recursive(N),
-    "\tTime:",
-    f'{timeit.timeit("fibonacci_recursive(N)", setup=f"from __main__ import fibonacci_recursive;N={N}", number=RUNS,):5f}',
-    "O(2^n)\tSpace: O(n)",
-)
+    print("\nRecursive Fibonacci numbers:")
+    for i in range(10):
+        print(recursive_fibonacci(i))
+        
+    print("\nTime complexity analysis:")
+    print(analyze_time_complexity(10))
+    print("\nSpace complexity analysis:")
+    print(analyze_space_complexity(10))
 
-
- 
+if __name__ == "__main__":
+    main()
